8ebc1d93b2f0204a7022a4753fc27ec4dac8ca53
david <dweke12@gmail.com>
2026-04-24 15:36:35 +0300

    weather forecast

--- src/pages/DroughtMonitoringPage.tsx
+++ src/pages/DroughtMonitoringPage.tsx
@@ -91,7 +91,7 @@ export default function DroughtMonitoringPage({
           </div>
         )}
         <iframe
-          src="https://front-end-cdi-c7ob.vercel.app"
+          src="https://cdi.rosewillbome.com"
           className="w-full h-full border-0"
           title="CDI Drought Monitoring System"
           onLoad={() => setIframeLoaded(true)}
